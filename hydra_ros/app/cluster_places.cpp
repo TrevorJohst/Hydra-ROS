f6df42f30981997b735f5d41755e7adc1b924861
lschmid <lschmid@mit.edu>
2025-09-17 20:00:01 -0400

    update places clusterin to use hydra infratructure

--- hydra_ros/app/cluster_places.cpp
+++ hydra_ros/app/cluster_places.cpp
@@ -16,6 +16,7 @@
 #include <glog/logging.h>
 #include <hydra/openset/embedding_distances.h>
 #include <hydra/rooms/room_finder.h>
+#include <hydra/utils/cognition_labels.h>
 #include <ianvs/node_init.h>
 #include <spark_dsg/dynamic_scene_graph.h>
 #include <spark_dsg/scene_graph_layer.h>
@@ -38,13 +39,10 @@ struct NodeConfig {
   bool interactive = true;
 };
 
-enum class FeatureType { CLIP = 0, SENTENCE = 1, BOTH = 2 };
-
 struct Config {
   std::string src_path;
   std::string dst_path;
-  FeatureType feature_type = FeatureType::BOTH;
-  config::VirtualConfig<hydra::EmbeddingDistance> distance_metric;
+  CognitionLabels::Config cognition_labels;
   std::string places_layer = DsgLayers::TRAVERSABILITY;
   hydra::RoomFinderConfig room_finder;
 };
@@ -61,12 +59,7 @@ void declare_config(Config& config) {
   name("ClusterPlaces::Config");
   field(config.src_path, "src_path");
   field(config.dst_path, "dst_path");
-  enum_field(config.feature_type,
-             "feature_type",
-             {{FeatureType::CLIP, "CLIP"},
-              {FeatureType::SENTENCE, "SENTENCE"},
-              {FeatureType::BOTH, "BOTH"}});
-  field(config.distance_metric, "distance_metric");
+  field(config.cognition_labels, "cognition_labels");
   field(config.places_layer, "places_layer");
   field(config.room_finder, "room_finder");
 }
@@ -83,11 +76,8 @@ class Clusterer {
       return;
     }
 
-    // computeFeatures.
-    const bool features_updated = computeFeatures(config);
-    if (features_updated || !prev_config_.distance_metric != config.distance_metric) {
-      computeDistances(config);
-    }
+    // compute features and distances.
+    computeFeatures(config);
 
     // Cluster.
     cluster(config);
@@ -105,92 +95,53 @@ class Clusterer {
   const hydra::DsgSender sender_;
   DynamicSceneGraph::Ptr graph_;
   Config prev_config_;
-  FeatureMap<int> node_features_;
+  std::unique_ptr<LazyCognitionLabels> labels_;
 
-  bool computeFeatures(const Config& config) {
-    if (!node_features_.empty() && config.feature_type == prev_config_.feature_type) {
-      return false;
-    }
-    node_features_.clear();
-    size_t num_invalid = 0;
-
-    // Extract the feature map.
-    const auto& meta_data = graph_->metadata.get()["features"];
-    for (const auto& [key, value] : meta_data.items()) {
-      const int id = std::stoi(key);
-      FeatureVector feature;
-      switch (config.feature_type) {
-        case FeatureType::CLIP:
-          feature = getClipFeature(value);
-          if (feature.size() == 0) {
-            num_invalid++;
-            continue;
-          }
-          break;
-        case FeatureType::SENTENCE:
-          feature = getSentenceFeature(value);
-          if (feature.size() == 0) {
-            num_invalid++;
-            continue;
-          }
-          break;
-        case FeatureType::BOTH: {
-          const auto clip_feature = getClipFeature(value);
-          const auto sentence_feature = getSentenceFeature(value);
-          if (clip_feature.size() == 0 || sentence_feature.size() == 0) {
-            num_invalid++;
-            continue;
-          }
-          feature.resize(clip_feature.size() + sentence_feature.size());
-          feature << clip_feature, sentence_feature;
-          break;
-        }
-        default:
-          LOG(ERROR) << "Unhandled feature type.";
-          return false;
-      }
-      node_features_[id] = feature;
+  void computeFeatures(const Config& config) {
+    // Check for feature changes.
+    if (!labels_ || config.cognition_labels.feature_type !=
+                        prev_config_.cognition_labels.feature_type) {
+      labels_ = std::make_unique<LazyCognitionLabels>(*graph_);
+    } else if (config.cognition_labels.distance_metric ==
+               prev_config_.cognition_labels.distance_metric) {
+      // No feature or metric changes.
+      return;
     }
 
-    // Remap invalid features to zero vectors.
-    const int feature_dim = node_features_.begin()->second.size();
-    LOG(INFO) << "Read " << (node_features_.size() + num_invalid)
-              << " features, of which " << num_invalid
-              << " invalid. Feature dim: " << feature_dim;
-    return true;
-  }
-
-  FeatureVector getClipFeature(const nlohmann::json& meta) {
-    if (!meta.contains("clip_feature") || meta["clip_feature"].is_null()) {
-      return {};
-    }
-    const auto clip_vec = meta["clip_feature"].get<std::vector<float>>();
-    if (clip_vec.empty()) {
-      return {};
-    }
-    return Eigen::Map<const FeatureVector>(clip_vec.data(), clip_vec.size());
-  }
-
-  FeatureVector getSentenceFeature(const nlohmann::json& meta) {
-    if (!meta.contains("sentence_embedding_feature") ||
-        meta["sentence_embedding_feature"].is_null()) {
-      return {};
-    }
-    const auto sentence_vec =
-        meta["sentence_embedding_feature"].get<std::vector<float>>();
-    if (sentence_vec.empty()) {
-      return {};
-    }
-    return Eigen::Map<const FeatureVector>(sentence_vec.data(), sentence_vec.size());
-  }
-
-  void computeDistances(const Config& config) {
-    const auto metric = config.distance_metric.create();
+    CognitionLabels::setup(config.cognition_labels);
     const auto& layer = graph_->getLayer(config.places_layer);
 
+    size_t num_with_id = 0;
+    size_t num_with_feature = 0;
+    // Parse all nodes.
     for (const auto& [_, node] : layer.nodes()) {
-      node->attributes<TraversabilityNodeAttributes>().distance = 0.0;
+      auto& attrs = node->attributes<TraversabilityNodeAttributes>();
+      attrs.distance = 0.0;
+      attrs.is_active = false;  // Hijack the active flag for valid nodes.
+
+      // Check if we have an ID.
+      if (attrs.cognition_labels.empty()) {
+        attrs.semantic_label = 0;  // No Label attached.
+        attrs.color = Color::gray();
+        continue;
+      }
+
+      // Check if we have a feature.
+      num_with_id++;
+      attrs.semantic_label = getMaxCognitionLabel(attrs.cognition_labels);
+      const auto& feature = labels_->get(attrs.semantic_label);
+      if (feature.size() == 0) {
+        attrs.color = Color::red();
+        continue;
+      }
+      num_with_feature++;
+      // NOTE(lschmid): Can add PCA or other feature visualization here.
+      attrs.color = Color::green();
+      attrs.is_active = true;
     }
+    LOG(INFO) << "Parsed " << layer.nodes().size() << " nodes, of which " << num_with_id
+              << " had a assigned ID and " << num_with_feature
+              << " had valid features.";
 
     // Compute the edge similarities.
     size_t num_invalid = 0;
@@ -199,28 +150,25 @@ class Clusterer {
     for (auto& [_, edge] : layer.edges()) {
       edge.info->weighted = true;
       const auto& source_node = layer.getNode(edge.source);
-      auto& source_attrs = source_node.attributes<TraversabilityNodeAttributes>();
-      const auto source_feature_it =
-          node_features_.find(source_attrs.cognition_labels.begin()->first);
-      if (source_feature_it == node_features_.end()) {
+      if (!source_node.is_active) {
         edge.info->weight = 0.0f;
         num_invalid++;
         continue;
       }
 
       const auto& target_node = layer.getNode(edge.target);
-      auto& target_attrs = target_node.attributes<TraversabilityNodeAttributes>();
-      const auto target_feature_it =
-          node_features_.find(target_attrs.cognition_labels.begin()->first);
-      if (target_feature_it == node_features_.end()) {
+      if (!target_node.is_active) {
         edge.info->weight = 0.0f;
         num_invalid++;
         continue;
       }
 
       // Compute the similarities.
-      const double score = 1.0 - 0.5 * metric->score(source_feature_it->second,
-                                                     target_feature_it->second);
+      auto& source_attrs = source_node.attributes<TraversabilityNodeAttributes>();
+      auto& target_attrs = target_node.attributes<TraversabilityNodeAttributes>();
+      const double score =
+          CognitionLabels::getScore(labels_->get(source_attrs.semantic_label),
+                                    labels_->get(target_attrs.semantic_label));
       edge.info->weight = score;
       source_attrs.distance = std::max(source_attrs.distance, score);
       target_attrs.distance = std::max(target_attrs.distance, score);
@@ -245,9 +193,8 @@ class Clusterer {
     }
 
     LOG(INFO) << "Computed scores for " << layer.edges().size() << " edges, of which "
-              << num_invalid << " invalid. "
-              << "min: " << min_score << ", max: " << max_score << ", mean: " << mean
-              << ", stddev: " << stddev;
+              << num_invalid << " invalid. min: " << min_score << ", max: " << max_score
+              << ", mean: " << mean << ", stddev: " << stddev;
   }
 
   void cluster(const Config& config) {
@@ -299,7 +246,6 @@ class Clusterer {
                  << "', cannot cluster places.";
       return false;
     }
-    node_features_.clear();
     return true;
   }
 
